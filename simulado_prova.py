931f1a30e36e7d66984f8441707d1c2cfd518a93
Guilherme <guilhermevmts@gmail.com>
2024-09-19 08:58:59 -0300

    bananinha

--- simulado_prova.py
+++ simulado_prova.py
@@ -4,10 +4,21 @@ tipos_cores = {
     'secundaria': ['verde', 'laranja', 'roxo'],
     }
 
+for tipo, cores in tipos_cores.items():
+    print(f"Cores {tipo}:")
+    for cor in cores:
+        print(f" - {cor}")
 
 # 2 - Faca um for loop na lista e mostre qual tipo ela é
 lst_cores = ['vermelho', 'roxo', 'preto'] 
-
+for cor in lst_cores:
+    for tipo, cores in tipos_cores.items():
+        if cor in cores:
+            print(f"A cor {cor} é {tipo}.")
+            encontrado = True
+            break
+    if not encontrado:
+        print(f"A cor {cor} não pertence a nenhum tipo conhecido.")
 
 
 
@@ -24,8 +35,15 @@ precos = {
 
 compra = ['luva', 'blusa', 'tenis']
 
+total_compra = 0
+for item in compra:
+    if item in precos:
+        total_compra += precos[item]
 
-# 4 – Faca uma funcao que entre 3 notas e calcule a media final de acordo com o crirterio de nota do ibmec
+print(f"O total da compra é: R$ {total_compra:.2f}")
+
+# 4 – Faca uma funcao que entre 3 notas e calcule a media
+#  final de acordo com o crirterio de nota do ibmec
 # Crie uma pasta meu_pacote e grava essa funcao la dentro
 # Importe a funcao, e chame ela, calculando a media final
 p1 = 8.5
